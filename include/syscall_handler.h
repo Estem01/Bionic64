d1a2c9458816ce418f06854f80e73ce18e3618b8
Estem0 <101932306+Estem01@users.noreply.github.com>
2025-03-25 02:40:02 -0300

    Syscall Table

--- include/syscall_handler.h
+++ include/syscall_handler.h
@@ -8,11 +8,12 @@
 class SyscallHandler {
 public:
     SyscallHandler();
-    uint64_t HandleSyscall(uint64_t syscall_num, uint64_t* args);
+    uint64_t HandleSyscall(uint64_t syscall_num, uint64_t* args, bool is_32bit);
 
 private:
     void MapBionicCalls();
-    std::unordered_map<uint64_t, std::function<uint64_t(uint64_t*)>> syscall_table_;
+    std::unordered_map<uint64_t, std::function<uint64_t(uint64_t*, bool)>> syscall_table_32bit;
+    std::unordered_map<uint64_t, std::function<uint64_t(uint64_t*, bool)>> syscall_table_64bit;
 };
 
 #endif
