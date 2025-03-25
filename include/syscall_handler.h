e8e104fd9d8a69cf79556e83a8408fecc50d4755
Estem0 <101932306+Estem01@users.noreply.github.com>
2025-03-25 02:31:52 -0300

    Update syscall_handler.h

--- include/syscall_handler.h
+++ include/syscall_handler.h
@@ -2,6 +2,8 @@
 #define SYSCALL_HANDLER_H
 
 #include <cstdint>
+#include <unordered_map>
+#include <functional>
 
 class SyscallHandler {
 public:
@@ -10,6 +12,7 @@ public:
 
 private:
     void MapBionicCalls();
+    std::unordered_map<uint64_t, std::function<uint64_t(uint64_t*)>> syscall_table_;
 };
 
 #endif
